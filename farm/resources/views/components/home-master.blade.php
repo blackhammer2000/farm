0d041adb8759665e98b85954dee3e1a434d15baa
wawerusamuel <waweruzamuel@gmail.com>
2023-02-22 16:27:31 +0300

    removed a duplicate active class...

--- farm/resources/views/components/home-master.blade.php
+++ farm/resources/views/components/home-master.blade.php
@@ -68,7 +68,7 @@
             <div class="navbar-nav mx-auto py-0">
                 <a href="{{route('home.index')}}" class="nav-item nav-link active">Home</a>
                 <a href="{{route('home.about')}}" class="nav-item nav-link">About</a>
-                <a href="{{route('home.service')}}" class="nav-item nav-link active">Service</a>
+                <a href="{{route('home.service')}}" class="nav-item nav-link">Service</a>
                 <a href="{{route('home.product')}}" class="nav-item nav-link">Product</a>
                 <div class="nav-item dropdown">
                     <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Pages</a>
