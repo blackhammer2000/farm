dbd97b57f0496ae2cab7fdc3add9bcad9602d8f5
wawerusamuel <waweruzamuel@gmail.com>
2023-02-22 16:34:32 +0300

    added some spacing for better legibility...

--- farm/routes/web.php
+++ farm/routes/web.php
@@ -19,22 +19,22 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home.index');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index']) -> name('home.index');
 
-Route::get('/about', [App\Http\Controllers\HomeController::class, 'about'])->name('home.about');
+Route::get('/about', [App\Http\Controllers\HomeController::class, 'about']) -> name('home.about');
 
-Route::get('/service', [App\Http\Controllers\HomeController::class, 'service'])->name('home.service');
+Route::get('/service', [App\Http\Controllers\HomeController::class, 'service']) -> name('home.service');
 
-Route::get('/product', [App\Http\Controllers\HomeController::class, 'product'])->name('home.product');
+Route::get('/product', [App\Http\Controllers\HomeController::class, 'product']) -> name('home.product');
 
-Route::get('/contact', [App\Http\Controllers\HomeController::class, 'contact'])->name('home.contact');
+Route::get('/contact', [App\Http\Controllers\HomeController::class, 'contact']) -> name('home.contact');
 
-Route::get('/blog', [App\Http\Controllers\HomeController::class, 'blog'])->name('home.blog');
+Route::get('/blog', [App\Http\Controllers\HomeController::class, 'blog']) -> name('home.blog');
 
-Route::get('/blog/detail', [App\Http\Controllers\HomeController::class, 'detail'])->name('home.detail');
+Route::get('/blog/detail', [App\Http\Controllers\HomeController::class, 'detail']) -> name('home.detail');
 
-Route::get('/features', [App\Http\Controllers\HomeController::class, 'features'])->name('home.features');
+Route::get('/features', [App\Http\Controllers\HomeController::class, 'features']) -> name('home.features');
 
-Route::get('/team', [App\Http\Controllers\HomeController::class, 'team'])->name('home.team');
+Route::get('/team', [App\Http\Controllers\HomeController::class, 'team']) -> name('home.team');
 
-Route::get('/testimonial', [App\Http\Controllers\HomeController::class, 'testimonial'])->name('home.testimonial');
+Route::get('/testimonial', [App\Http\Controllers\HomeController::class, 'testimonial']) -> name('home.testimonial');
