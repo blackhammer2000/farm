84d374001f7634f51536718ce6ded640f36d8bc7
wawerusamuel <waweruzamuel@gmail.com>
2023-02-22 14:46:24 +0300

    created the necessary routes...

--- farm/routes/web.php
+++ farm/routes/web.php
@@ -20,3 +20,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home.index');
+
+Route::get('/about', [App\Http\Controllers\HomeController::class, 'about'])->name('home.about');
